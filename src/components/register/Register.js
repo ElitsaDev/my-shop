c5cbf63df07140136c629c0425330c3bca0a1ca8
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-03-25 12:59:24 +0200

    added register form

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -13,14 +13,17 @@ export default function Register() {
                             <div className={styles.error}>Email or Password are not valid.</div>
                             <label className={styles.label}>E-mail: </label> 
                             <input type="text" name="email"/> 
-                            <label>Username: <input type="text" name="username"/></label> 
-                            <label>Password: <input type="password" name="password"/></label> 
-                            <label>Repeat: <input type="password" name="repass"/></label>
-                            <input className="action cta" type="submit" value="Create Account"/>
+                            <label className={styles.label}>Username: </label> 
+                            <input type="text" name="username"/>
+                            <label className={styles.label}>Password: </label> 
+                            <input type="password" name="password"/>
+                            <label className={styles.label}>Repeat password: </label> 
+                            <input type="password" name="repass"/>
+                            <input className={styles.btn} type="submit" value="Create Account"/>
                     </form>
                     
-                    <footer className="pad-small">Already have an account? <a href="/login" className="invert">Sign in here</a>
-                    </footer>
+                    <div className="pad-small">Already have an account? <a href="/login" className="invert">Sign in here</a>
+                    </div>
                 </article>
             </section>
         </>
